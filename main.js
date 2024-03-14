2fc8f476e1c1aad16745bcef0a030c72b46177d7
Mukhammadaziz <rustamov977075073@gmail.com>
2024-03-14 15:57:40 +0500

    10-dars

---
+++ main.js
@@ -0,0 +1,57 @@
+// function test(a,b,c) {
+//     console.log(a+b+c);
+// }
+
+// test(10, 20, 30);
+
+// function test(a,b) {
+//     return a*b;
+// }
+
+// let res = test(1,2);
+// console.log(res);
+
+
+// let test = function(a,b) {
+//     return a+b
+// }
+
+// console.log(test(20,30));
+
+// const test =(a,b)=> {
+//     console.log(a+b);
+// }
+
+// test(30,40);
+
+// let test = (a,b)=> {
+//     return a**b;
+// }
+// console.log(test(20,3));
+
+// let test = (a)=> {
+//     if (a === 'Mukhammadaziz') {
+//         console.log('salom '+ a);
+//     } else {
+//         console.log('ismni hato kiritdingiz');
+//     }
+// }
+
+// test('Mukhammadaziz')
+
+
+// function test(a) {
+//     return Math.sqrt(a);
+// }
+
+// console.log(test(49));
+
+function kattaQiymatniChiqarish(a, b) {
+    if (a > b) {
+        alert(a + " eng katta qiymat!");
+    } else {
+        alert(b + " eng katta qiymat!");
+    }
+}
+
+kattaQiymatniChiqarish(7, 15);
